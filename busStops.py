d77725b2972517020f5ad0da99d2c4893fc78a43
jackspore <sporeyang@live.cn>
2018-12-04 21:54:28 +0800

    class BusStop should remember all stops in network

--- busStops.py
+++ busStops.py
@@ -2,10 +2,13 @@
 
 class BusStop:
 
+    listAllStops = []
+
     def __init__(self, _id, _listBus):
         self.id = _id
         self.listBus = _listBus
         self.switchStop = False
+        BusStop.listAllStops.append(self)
 
     def addBus(self, _newBus):
         self.listBus.append(_newBus)
